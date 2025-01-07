5df0c29cab2f74e493e5a9d64f52211f48983e55
miro <hi@miro.im>
2025-01-07 16:48:36 +0800

    add rgb_from_yuv

--- crates/kornia-imgproc/src/color/mod.rs
+++ crates/kornia-imgproc/src/color/mod.rs
@@ -4,4 +4,4 @@ mod yuv;
 
 pub use gray::{bgr_from_rgb, gray_from_rgb, rgb_from_gray};
 pub use hsv::hsv_from_rgb;
-pub use yuv::yuv_from_rgb;
+pub use yuv::{rgb_from_yuv, yuv_from_rgb};
--- crates/kornia-imgproc/src/color/yuv.rs
+++ crates/kornia-imgproc/src/color/yuv.rs
@@ -14,9 +14,9 @@ use kornia_image::{Image, ImageError};
 ///
 /// The YUV image with the following channels:
 ///
-/// * Y: The luma channel in the range [0, 1.0].
-/// * U: The blue-difference chroma channel in the range [-0.436, 0.436].
-/// * V: The red-difference chroma channel in the range [-0.615, 0.615].
+/// * Y: The luma channel.
+/// * U: The blue-difference chroma channel.
+/// * V: The red-difference chroma channel.
 ///
 /// Precondition: the input image must have 3 channels.
 /// Precondition: the output image must have 3 channels.
@@ -74,6 +74,71 @@ pub fn yuv_from_rgb(src: &Image<f32, 3>, dst: &mut Image<f32, 3>) -> Result<(), 
     Ok(())
 }
 
+/// Convert an YUV image to an RGB image.
+///
+/// The input image is assumed to have 3 channels in the order Y, U, V.
+///
+/// # Arguments
+///
+/// * `src` - The input YUV image assumed to have 3 channels.
+/// * `dst` - The output RGB image.
+///
+/// Precondition: the input image must have 3 channels.
+/// Precondition: the output image must have 3 channels.
+/// Precondition: the input and output images must have the same size.
+///
+/// # Example
+///
+/// ```
+/// use kornia_image::{Image, ImageSize};
+/// use kornia_imgproc::color::rgb_from_yuv;
+///
+/// let image = Image::<f32, 3>::new(
+///     ImageSize {
+///        width: 4,
+///        height: 5,
+///     },
+///     vec![0f32; 4 * 5 * 3],
+/// )
+/// .unwrap();
+///
+/// let mut rgb = Image::<f32, 3>::from_size_val(image.size(), 0.0).unwrap();
+///
+/// rgb_from_yuv(&image, &mut rgb).unwrap();
+///
+/// assert_eq!(rgb.num_channels(), 3);
+/// assert_eq!(rgb.size().width, 4);
+/// assert_eq!(rgb.size().height, 5);
+/// ```
+pub fn rgb_from_yuv(src: &Image<f32, 3>, dst: &mut Image<f32, 3>) -> Result<(), ImageError> {
+    if src.size() != dst.size() {
+        return Err(ImageError::InvalidImageSize(
+            src.cols(),
+            src.rows(),
+            dst.cols(),
+            dst.rows(),
+        ));
+    }
+
+    // compute the YUV values
+    parallel::par_iter_rows(src, dst, |src_pixel, dst_pixel| {
+        // Normalize the input to the range [0, 1]
+        let y = src_pixel[0];
+        let u = src_pixel[1];
+        let v: f32 = src_pixel[2];
+
+        let r = y + 1.14 * v;
+        let g = y - 0.395 * u - 0.581 * v;
+        let b = y + 2.032 * u;
+
+        dst_pixel[0] = r;
+        dst_pixel[1] = g;
+        dst_pixel[2] = b;
+    });
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use kornia_image::{Image, ImageError, ImageSize};
@@ -115,7 +180,44 @@ mod tests {
         assert_eq!(yuv.size().height, 2);
 
         for (a, b) in yuv.as_slice().iter().zip(expected.iter()) {
-            assert!((a - b).abs() < 1e-6f32);
+            // assert!((a - b).abs() < 1e-6f32);
+            assert_eq!(a, b);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn rgb_from_yuv() -> Result<(), ImageError> {
+        #[rustfmt::skip]
+        let image = Image::<f32, 3>::new(
+            ImageSize {
+                width: 1,
+                height: 2,
+            },
+            vec![
+                0.299, -0.147, 0.615,
+                0.587, -0.289, -0.515,
+            ],
+        )?;
+
+        #[rustfmt::skip]
+        let expected = [
+            255.0, 0.0, 0.0,
+            0.0, 255.0, 0.0,
+        ];
+
+        let mut rgb = Image::<f32, 3>::from_size_val(image.size(), 0.0)?;
+
+        super::rgb_from_yuv(&image, &mut rgb)?;
+
+        assert_eq!(rgb.num_channels(), 3);
+        assert_eq!(rgb.size().width, 1);
+        assert_eq!(rgb.size().height, 2);
+
+        for (a, b) in rgb.as_slice().iter().zip(expected.iter()) {
+            assert!((a - b).abs() < 1e-1f32);
+            // assert_eq!(a, b);
         }
 
         Ok(())
